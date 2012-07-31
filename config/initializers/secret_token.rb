45c02777667602e174707470df05c3d3030d70a8
kongo <d.parshenko@gmail.com>
2012-07-31 11:49:28 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieClub::Application.config.secret_token = 'e672e8be52e9538d7b31269405bd6750d34b9e707846526c76726b8e55e976266c29c33bbf023df7dcc364c8fe2327056e57d0b3e215fd789fe03aff9ba56f82'
