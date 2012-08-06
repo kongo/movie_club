c2cb423a673db7d048c44ecdcbc56427e14fcd4c
kongo <d.parshenko@gmail.com>
2012-08-06 15:00:00 +0300

    Added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users_data = [
+  {
+      username: 'dkob',
+      fullname: 'Denys Kobylins\'kyi',
+      email:    'dkob@softserveinc.com',
+      password: 'password',
+      is_admin: true
+  },
+  {
+      username: 'dparsh',
+      fullname: 'Dmytro Parshenko',
+      email:    'dparsh@softserveinc.com',
+      password: 'password',
+      is_admin: true
+  },
+  {
+      username: 'testuser',
+      fullname: 'Test User',
+      email:    'testuser@example.com',
+      password: 'password',
+      is_admin: false
+  }
+]
+
+users_data.each do |data|
+  u = User.new
+  data.each { |k,v| u.send "#{k}=", v }
+  u.save
+end
