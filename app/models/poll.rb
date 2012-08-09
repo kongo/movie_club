2d5b13a0dba8bd7fcd38285f09e861e8ce86c6fe
kongo <d.parshenko@gmail.com>
2012-08-09 10:18:56 +0300

    Admin polls index view

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -11,10 +11,21 @@ class Poll < ActiveRecord::Base
     end
   end
 
+  def open?
+    Time.zone.now < self.ends_at
+  end
+
+  def closed?
+    !self.open?
+  end
+
   private
 
   def default_values
-    self.title    ||= "Choice for " + Date.commercial(Date.today.year, Date.today.cweek, 4).to_s(:long_ordinal)
-    self.ends_at  ||= Date.commercial(Date.today.year, Date.today.cweek, 4) - 1.day
+    target_date   = Date.commercial(Date.today.year, Date.today.cweek, 4)
+    self.title    ||= "Choice for " + target_date.to_s(:long_ordinal)
+    self.starts_at = Time.zone.now
+    raise self.starts_at.class.inspect
+    self.ends_at  ||= target_date.beginning_of_day
   end
 end
