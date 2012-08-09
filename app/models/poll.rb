f820243598fed0f0cdff589aef207647b681efac
kongo <d.parshenko@gmail.com>
2012-08-09 11:25:36 +0300

    Set poll's default ends_at date

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ActiveRecord::Base
   attr_accessible :ends_at, :starts_at, :title, :options, :options_attributes
   after_initialize :default_values
 
-  has_many :options
+  has_many :options, :dependent => :destroy
   accepts_nested_attributes_for :options
 
   default_scope ->{ order("ends_at desc") }
@@ -27,7 +27,6 @@ class Poll < ActiveRecord::Base
     target_date   = Date.commercial(Date.today.year, Date.today.cweek, 4)
     self.title    ||= "Choice for " + target_date.to_s(:long_ordinal)
     self.starts_at = Time.zone.now
-    raise self.starts_at.class.inspect
-    self.ends_at  ||= target_date.beginning_of_day
+    self.ends_at  ||= (target_date - 1.day).to_datetime.in_time_zone.change({hour: 16, min: 30})
   end
 end
