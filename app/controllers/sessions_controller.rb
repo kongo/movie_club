7895aa5d968be7dad0d10d56027bee01ccd51ad8
kongo <d.parshenko@gmail.com>
2012-08-20 15:17:56 +0300

    Fixed remember_me option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
 
   def create
     if user = UserFinder.find(params[:user][:username], params[:user][:password])
-      remember_me user
       sign_in user
+      remember_me user
       redirect_to root_path
     else
       flash[:error] = 'Username or password is incorrect :('
