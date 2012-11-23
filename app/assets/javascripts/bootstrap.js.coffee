7892f33d73910638fbbbbf6f996f1ffe556d5e69
kongo <d.parshenko@gmail.com>
2012-11-23 18:20:04 +0200

    "I will come" checkbox

--- app/assets/javascripts/bootstrap.js.coffee
+++ app/assets/javascripts/bootstrap.js.coffee
@@ -2,3 +2,12 @@ jQuery ->
   $("a[rel=popover]").popover()
   $(".tooltip").tooltip()
   $("a[rel=tooltip]").tooltip()
+  $("#vote_will_attend").on(
+    "change",
+    ->
+      btn = $(".poll.open .btn-primary")
+      if $(@).is(":checked")
+        btn.removeAttr("disabled")
+      else
+        btn.attr("disabled", "disabled")
+  )
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,13 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :option
+  attr_accessor :will_attend
+  attr_accessible :will_attend
   attr_accessible :user, :option
 
   validates_presence_of :user
   validates_presence_of :option
+  validates :will_attend, :inclusion => {:in => ["1", :"1", 1]}
 
   scope :with_option, ->(option) { where(option_id: option.id) }
 end
